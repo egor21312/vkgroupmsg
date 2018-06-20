81499333a298c86d208c0a39b93edb7645aca1fb
egor21312 <anton255260@gmail.com>
2018-06-20 18:56:56 +0300

    Release of chat view! (only as user)

---
+++ python/gethistory.py
@@ -0,0 +1,37 @@
+import vk
+import time
+import math
+session = vk.Session(access_token='6bf8e7e4baacb7b4c530f8cfb1b85dd32a48e910b2b34fb317911e9360f8251ebf98737d597022a73c1f8')
+vk_api = vk.API(session)
+c = 10
+oldmsg = 0
+pid = int(input("Введите id беседы > "))
+peer = 2000000000 + pid
+while c > 0:
+	history = vk_api.messages.getHistory(count=1, peer_id=peer, v='5.80')
+	id = history['items'][0]['from_id']
+	if id > 0:
+		user = vk_api.users.get(user_ids=id, v='5.80')
+		first_name = user[0]['first_name']
+		last_name = user[0]['last_name']
+		text = history['items'][0]['text']
+		msg = first_name + ' ' + last_name + ' ' + '(' + '@' + 'id' + str(id) + ')' + ': ' + str(text)
+		if msg == oldmsg:
+ 			time.sleep(0.8)
+		else:
+			print(msg)
+			oldmsg = msg
+			time.sleep(0.8)
+	else:
+		gid = math.fabs(id)
+		modulegid = int(gid)
+		group = vk_api.groups.getById(group_ids=gid, v='5.80')
+		name = group[0]['name']
+		text = history['items'][0]['text']
+		msg = name + ' ' + '(' + '@' + 'club' + str(modulegid) + ')' + ': ' + str(text)
+		if msg == oldmsg:
+			time.sleep(0.8)
+		else:
+			print(msg)
+			oldmsg = msg
+			time.sleep(0.8)
